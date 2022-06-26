8704c5854291da4b4b748c77da1b5b2660eb1d99
QuentinCode <quentin.gibouin@gmail.com>
2022-06-26 21:46:21 +0200

    Test des anims Js

---
+++ App.js
@@ -0,0 +1,11 @@
+const cards = document.querySelectorAll('.card')
+
+window.addEventListener('load', initAnim)
+
+function initAnim(){
+    
+    const TLFADE = gsap.timeline();
+
+    TFLADE
+    .to(title, {opacity: 1, delay: 0.2})
+}
